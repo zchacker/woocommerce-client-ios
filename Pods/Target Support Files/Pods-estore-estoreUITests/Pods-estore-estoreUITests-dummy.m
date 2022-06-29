80e52bd9ac0c83cfaad168fdf0e49b5c0c2807c5
zchacker <ice18@windowslive.com>
2022-06-29 20:44:50 +0300

    hello

---
+++ Pods/Target Support Files/Pods-estore-estoreUITests/Pods-estore-estoreUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_estore_estoreUITests : NSObject
+@end
+@implementation PodsDummy_Pods_estore_estoreUITests
+@end
---
+++ Pods/Target Support Files/Pods-estore/Pods-estore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_estore : NSObject
+@end
+@implementation PodsDummy_Pods_estore
+@end
