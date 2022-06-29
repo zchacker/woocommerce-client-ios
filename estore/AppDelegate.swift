6544f2a6666dc9321de05cf27268d103187b83a6
zchacker <ahmedadmnagem@gmail.com>
2022-06-29 20:28:03 +0300

    change name

--- estore/AppDelegate.swift
+++ estore/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    
     // MARK: UISceneSession Lifecycle
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
